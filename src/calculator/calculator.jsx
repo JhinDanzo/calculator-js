b6e7edc4c4408184a7e1cab470007a44ee351bf4
JhinDanzo <gentj03@icloud.com>
2024-06-16 22:18:34 +0300

    buttons-click

--- src/calculator/calculator.jsx
+++ src/calculator/calculator.jsx
@@ -1,39 +1,25 @@
-import '../calculator/calculator.css'
-import { useState, useRef } from 'react';
+import '../calculator/calculator.css';
+import { useState } from 'react';
 
-function Calculator () {
+function Calculator() {
+  const [screenNumber, setScreenNumber] = useState('0');
 
-    const [screenNumber, setScreenNumber] = useState(0);
-    const divRefs = useRef(null);
+  const handleButtonClick = (value) => {
+    setScreenNumber((prev) => prev + value);
+  };
 
-    const updateScreenNumber = () => {
-       setScreenNumber(this.textContent);
-    }
-
-    return(
-    <body>
-        <div className='calculator-body'>
-            <div className='calculator-screen'>{screenNumber}</div>
-            <div className='calculator-buttons'>
-                <button className='button'>+</button>
-                <button className='button'>-</button>
-                <button className='button'>=</button>
-                <button onClick={updateScreenNumber} className='button'>1</button>
-                <button className='button'>2</button>
-                <button className='button'>3</button>
-                <button className='button'>4</button>
-                <button className='button'>5</button>
-                <button className='button'>6</button>
-                <button className='button'>7</button>
-                <button className='button'>8</button>
-                <button className='button'>9</button>
-                <button className='button'>.</button>
-                <button className='button'>0</button>
-                <button className='button'>%</button>
-            </div>
-        </div>
-    </body>
-    )
+  return (
+    <div className='calculator-body'>
+      <div className='calculator-screen'>{screenNumber}</div>
+      <div className='calculator-buttons'>
+        {['+', '-', '=', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', '%', 'c', '00', 'ac'].map((button, index) => (
+          <button key={index} onClick={() => handleButtonClick(button)} className='button'>
+            {button}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
