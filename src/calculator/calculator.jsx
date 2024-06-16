694bbd795b11e426846ef0e8a01cd4af0822359e
JhinDanzo <gentj03@icloud.com>
2024-06-16 22:30:58 +0300

    AC done

--- src/calculator/calculator.jsx
+++ src/calculator/calculator.jsx
@@ -2,21 +2,36 @@ import '../calculator/calculator.css';
 import { useState } from 'react';
 
 function Calculator() {
-  const [screenNumber, setScreenNumber] = useState('0');
+  const [screenNumber, setScreenNumber] = useState('');
 
   const handleButtonClick = (value) => {
     setScreenNumber((prev) => prev + value);
   };
 
+  const handleAcClick = () => {
+    setScreenNumber('');
+  }
+
+  const handleCClick = (value) => {
+    setScreenNumber(screenNumber - value)
+  }
+
+  const handlePercentClick = () => {
+    setScreenNumber(screenNumber*0.01)
+  }
+
   return (
     <div className='calculator-body'>
       <div className='calculator-screen'>{screenNumber}</div>
       <div className='calculator-buttons'>
-        {['+', '-', '=', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', '%', 'c', '00', 'ac'].map((button, index) => (
+        {['+', '-', '=', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', '00'].map((button, index) => (
           <button key={index} onClick={() => handleButtonClick(button)} className='button'>
             {button}
           </button>
         ))}
+        <button className='button' onClick={handleAcClick}>ac</button>
+        <button className='button' onClick={handleCClick}>c</button>
+        <button className='button' onClick={handlePercentClick}>%</button>
       </div>
     </div>
   );
